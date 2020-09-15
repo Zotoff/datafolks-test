d4443c27e7b9c7b44fab5389475c99841306407d
Павел Зотов <paulzotoff@gmail.com>
2020-09-15 10:55:03 +0300

    Fixes

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -4,9 +4,9 @@ const plumber = require('gulp-plumber')
 const htmlValidator = require('gulp-w3c-html-validator')
 
 module.exports = function pug2html(cb) {
-	return gulp.src('src/pages/**/*.pug')
-	.pipe(plumber())
-	.pipe(pug())
-	.pipe(htmlValidator())
-	.pipe(gulp.dest('build'))
+        return gulp.src('src/pages/*.pug')
+        .pipe(plumber())
+        .pipe(pug())
+        .pipe(htmlValidator())
+        .pipe(gulp.dest('build'))
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 const sliderLinks = document.querySelectorAll(`a[data-toggle='modal']`)
 const sliderModal = document.querySelector(`.datafolks__modal`)
-const contactForm = document.querySelector(`form`)
+const forms = document.forms
+const contactForm = forms[0]
+
+const contactFormButton = contactForm.querySelector(`input[type="submit"]`)
+
+
 sliderLinks.forEach((item) => {
     item.addEventListener(`click`, (e) => {
         e.preventDefault()
@@ -17,7 +22,33 @@ sliderLinks.forEach((item) => {
 sliderModal.addEventListener(`click`, () => {
     sliderModal.classList.add(`d--none`)
 })
-contactForm.submit((e) => {
+
+contactFormButton.addEventListener(`click`, (e) => {
     e.preventDefault()
-    console.log(`submit`)
+
+    const contactFormName = contactForm.childNodes[0].querySelector(`input[type="text"]`).value
+    const contactFormEmail = contactForm.childNodes[1].querySelector(`input[type="email"]`).value
+    const contactFormPhone = contactForm.childNodes[2].querySelector(`input[type="tel"]`).value
+    const contactFormText = contactForm.childNodes[3].querySelector(`textarea`).value
+
+    const body = `name=` + encodeURIComponent(contactFormName) +
+    `&email=` + encodeURIComponent(contactFormEmail) + `&phone` + encodeURIComponent(contactFormPhone) + `&text` + encodeURIComponent(contactFormText)
+
+    /* TODO form XHR Request */
+
+    // const xhr = new XMLHttpRequest()
+    // xhr.open(`POST`, `../../form.php`, true)
+    // xhr.setRequestHeader(`Content-Type`, `application/x-www-form-urlencoded; charset=UTF-8`)
+
+    // xhr.onreadystatechange = function () {
+    //     if (this.readyState === 4) {
+    //         if (this.status >= 200 && this.status < 400) {
+    //             const response = this.responseText
+    //             console.log(response)
+    //         } else {
+    //             console.log(`Something went wrong`)
+    //         }
+    //     }
+    // }
+    // xhr.send(body)
 })
